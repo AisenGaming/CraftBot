1b0af96d720f46aec4ac05ca88dd45748f9c3f88
Aisen Nurmi Giorgi <31571936+AisenGaming@users.noreply.github.com>
2020-03-13 02:15:11 -0700

    Update bot.js
    Added more Config file options for more flexibility

--- Code/bot.js
+++ Code/bot.js
@@ -29,11 +29,12 @@ function command(help, message, args, ops, cmd) {
 // Ready Event -- Runs whenever the bot is launched.
 client.on('ready', () => {
     console.log('Launched!');
-    // TODO: Add config for this
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (config.activities_list.length - 1) + 1);
-        client.user.setActivity(config.activities_list[index]);
-    }, 10000);
+    if (config.botPlaying == true) {
+        setInterval(() => {
+            const index = Math.floor(Math.random() * (config.activities_list.length - 1) + 1);
+            client.user.setActivity(config.activities_list[index]);
+        }, config.botPlayingInterval);
+    }
 });
 
 // Listener Events
@@ -49,32 +50,36 @@ client.on('message', async message => {
     if (message.author.bot) return;
     
     // Check if Message contains a Discord Invite Link
-    // TODO: Add Config for this
-    if (message.content.toLowerCase().includes("https://discord.gg/")) {
-        message.delete();
-        message.author.send("Invites to other servers are NOT allowed. Speak to a member of the staff if you want to link your server.")
-            .then(msg => console.log(`Deleted invite code from ${msg.author.username}`))
-            .catch(console.error);
+    if (config.allowInviteLinks == false) {
+        if (message.content.toLowerCase().includes("https://discord.gg/")) {
+            message.delete();
+            message.author.send(config.inviteLinkWarning)
+                .then(msg => console.log(`Deleted invite code from ${msg.author.username}`))
+                .catch(console.error);
+        }
     }
     
     // Check if Message is in #Rules & if keywords are hit.
-    // TODO: Add Config for this
-    if (message.channel.id == config.RulesChannelID) {
-        if (message.content.toLowerCase().includes("i accept")) {
-        message.member.removeRole('467939264685146124')
-            .then(console.log(`${message.author.tag} has agreed to the rules. @ ${new Date()}`))
-            .catch(console.error);
-            const newMemberEmbed = new Discord.RichEmbed()
-                .setTitle("User Joined")
-                .setColor(0xe74c3c)
-                .setFooter("© Equinox Studios")
-                .setThumbnail(message.author.avatarURL || "http://pixelartmaker.com/art/ccb56d90dfd4f6e.png")
-                .setTimestamp(new Date())
-                .addField(`${message.author.tag} Joined the Server`, `Enjoy the crafting goodness!`);
-            client.channels.get(config.AnnouncementsID).send(newMemberEmbed);
+    if (config.GatewayEnabled == true) {
+        if (message.channel.id == config.GatewayChannelID) {
+            if (message.content.toLowerCase().includes("i accept")) {
+                message.member.removeRole(config.GatewayRoleID)
+                    .then(console.log(`${message.author.tag} has agreed to the rules. @ ${new Date()}`))
+                    .catch(console.error);
+                const newMemberEmbed = new Discord.RichEmbed()
+                    .setTitle("User Joined")
+                    .setColor(0xe74c3c)
+                    .setFooter("© Equinox Studios")
+                    .setThumbnail(message.author.avatarURL || "http://pixelartmaker.com/art/ccb56d90dfd4f6e.png")
+                    .setTimestamp(new Date())
+                    .addField(`${message.author.tag} Joined the Server`, `${config.JoinMessage}`);
+                client.channels.get(config.AnnouncementsID).send(newMemberEmbed);
+                message.delete()
+                    .then(console.log("Deleted I Accept message"));
+            }
+            message.delete()
+                .then(console.log("Deleted unrelated message in Gateway Channel")
         }
-        message.delete()
-            .then(console.log("Deleted I Accept message"));
     }
     
     // Command Handler
@@ -96,29 +101,31 @@ client.on('message', async message => {
     }
 });
 
-// TODO: Add Config to these events
-
 // New Member Alert
 client.on('guildMemberAdd', member => {
-    // Update Voice Channels with Server Stats
-    client.channels.get(config.MemberCountID).setName(`Member Count: ${member.guild.memberCount}`)
-        .then(console.log(`Updated Member Count on Server to ${member.guild.memberCount}`));
-    member.addRole('467939264685146124')
-        .then(console.log(`${member.user.tag}. Joined the server, yet to accept rules`))
-        .catch(console.error);
+    // Update Voice Channels with Server Stats if Config for it is enabled
+    if (config.newMemberAlert == true) {
+        client.channels.get(config.MemberCountID).setName(`Member Count: ${member.guild.memberCount}`)
+            .then(console.log(`Updated Member Count on Server to ${member.guild.memberCount}`));
+        member.addRole(config.GatewayRoleID)
+            .then(console.log(`${member.user.tag}. Joined the server, yet to accept rules`))
+            .catch(console.error);
+    }
 });
 
 // Leaving Member Message
 client.on('guildMemberRemove', member => {
     // Update Voice Channels with Server Stats
-    client.channels.get(config.MemberCountID).setName(`Member Count: ${member.guild.memberCount}`)
-        .then(console.log(`Updated Member Count on Server to ${member.guild.memberCount}`));
-    const oldMemberEmbed = new Discord.RichEmbed()
-        .setTitle("User Left")
-        .setColor(0xe74c3c)
-        .setFooter("© Equinox Studios")
-        .setThumbnail(member.avatarURL)
-        .setTimestamp(new Date())
-        .addField(`${member.user.tag} Left the Server`, `Your Flesh and Soul is now forfeit to the Octopus!`);
-    client.channels.get(config.AnnouncementsID).send(oldMemberEmbed);
+    if (config.memberLeavingAlert == true) {
+        client.channels.get(config.MemberCountID).setName(`Member Count: ${member.guild.memberCount}`)
+            .then(console.log(`Updated Member Count on Server to ${member.guild.memberCount}`));
+        const oldMemberEmbed = new Discord.RichEmbed()
+            .setTitle("User Left")
+            .setColor(0xe74c3c)
+            .setFooter("© Equinox Studios")
+            .setThumbnail(member.avatarURL)
+            .setTimestamp(new Date())
+            .addField(`${member.user.tag} Left the Server`, `${config.MemberLeavingMessage}`);
+        client.channels.get(config.AnnouncementsID).send(oldMemberEmbed);
+    }
 });
